3605e599ad7d71b8b62f5054b9eaa5169f2b02a4
fabrizioguespe <fabrizioguespe@MacFabri.local>
2023-06-14 11:35:07 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const ethers = require("ethers");
+const { Client } = require("@xmtp/xmtp-js");
+
+async function main() {
+  const xmtp = await Client.create(ethers.Wallet.createRandom(), {
+    env: "dev",
+  });
+  const conv = await xmtp.conversations.newConversation(
+    "0x3F11b27F323b62B159D2642964fa27C46C841897",
+  );
+  const message = await conv.send("gm");
+  console.log(message);
+}
+main();
